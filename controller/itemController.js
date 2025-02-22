48aef027270e9441f8497fc2160227d3887d5515
Kalana-indula <kalanasmedia@outlook.com>
2025-02-22 07:09:31 +0530

    find all items and find by id method added

--- controller/itemController.js
+++ controller/itemController.js
@@ -20,3 +20,35 @@ export const addItem=async (req,res)=>{
         });
     }
 }
+
+//find all items
+export const findAllItems=async (req,res)=>{
+    try{
+        //fetch all items
+        const items=await Item.find();
+
+        res.json(items);
+    }catch(err){
+        res.status(500).json({
+            "message":"Error findAllItems",
+            error:err.message
+        });
+    }
+}
+
+//find item by id
+export const findItemById=async (req,res)=>{
+    try{
+        //fetch 'itemId' from request parameters
+        const itemId=req.params.itemId;
+
+        const existingItem=await Item.findOne({itemId: itemId});
+        // console.log("Item Id: ",itemId);
+        res.json(existingItem);
+    }catch(err){
+        res.status(500).json({
+            "message":"error finding item",
+            "error":err.message
+        });
+    }
+}
--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {addItem} from "../controller/itemController.js";
+import {addItem, findAllItems, findItemById} from "../controller/itemController.js";
 
 //creating an itemRouter object
 const itemRouter = express.Router();
 
 itemRouter.post("/",addItem);
+itemRouter.get("/",findAllItems);
+itemRouter.get("/:itemId",findItemById);
 
 export default itemRouter;
