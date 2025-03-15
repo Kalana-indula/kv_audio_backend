061cd9f44ebde55a1384c4167b989a521f26b93d
Kalana-indula <kalanasmedia@outlook.com>
2025-03-15 15:02:03 +0530

    review model added

---
+++ models/review.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    rating:{
+        type:Number,
+        required:true
+    },
+    comment:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true,
+        default:Date.now()
+    },
+    profilePicture:{
+        type:String,
+        required:true,
+        default:"location_link"
+    },
+    isApproved:{
+        type:Boolean,
+        required:true,
+        default:false
+    }
+});
+
+const Review = mongoose.model("Review",reviewSchema);
+exports.Review = Review;
